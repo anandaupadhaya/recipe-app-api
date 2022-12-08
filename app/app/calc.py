bd6c861b2922de11c07e17a5dd9ae85e2bb591ce
Ananda_Upadhaya <Ananda.Upadhaya_ext@ltts.com>
2022-12-08 12:42:33 +0530

    DB Configuration

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator function
+
+"""
+
+def add(x, y):
+    """ this cal function returns addition of x, y """
+    return x + y
+
+def substract(r, s):
+    """ this cal function returns substraction of r, s """
+    return r - s
