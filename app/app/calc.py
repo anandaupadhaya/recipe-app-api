92e6ae355613e4df72d30907218765f7fa275171
Ananda_Upadhaya <Ananda.Upadhaya_ext@ltts.com>
2022-12-08 16:18:32 +0530

    updates

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,13 @@
 """
-calculator function
 
+calculator function
 """
+
 
 def add(x, y):
     """ this cal function returns addition of x, y """
     return x + y
+
 
 def substract(r, s):
     """ this cal function returns substraction of r, s """
